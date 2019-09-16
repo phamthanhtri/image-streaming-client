e77f9ecdc705097fbb6dbc02f91b251e4a5ffb80
hoatran <tranvanhoa533@gmail.com>
2019-09-16 14:18:46 +0700

    config camera

--- client.py
+++ client.py
@@ -8,12 +8,49 @@ import argparse
 import socket
 import time
 import cv2
+from matplotlib import pyplot as plt
+import subprocess
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--server-ip", required=True,
 	help="ip address of the server to which the client will connect")
 args = vars(ap.parse_args())
 
+
+def find(max_id):
+    """Find camera id."""
+    plt.ion()
+    plt.show()
+
+    cap = cv2.VideoCapture()
+
+    device = max_id
+    print('Finding camera device...')
+    while device >= 0:
+        cap.open(device)
+        if cap.isOpened():
+            print('Found a camera device, id: {}'.format(device))
+            # show preview
+            _, frame = cap.read()
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            plt.imshow(rgb_frame, aspect='equal', extent=None)
+            plt.draw()
+            plt.axis('off')
+            plt.pause(0.001)
+
+            cap.release()
+            break
+        device -= 1
+    else:
+        print('Cannot start video capture. Please connect camera and run again')
+
+    plt.close('all')
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return device
+
+	
 # initialize the ImageSender object with the socket address of the
 # server
 sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
@@ -27,14 +64,35 @@ sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
 # camera sensor to warmup
 rpiName = socket.gethostname()
 #vs = VideoStream(usePiCamera=True).start()
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
+camera_id = find(10)
+# camera_id = 0
+vs = VideoStream(src=camera_id).start()
+time.sleep(1.0)
+
+command = "v4l2-ctl -d {} --set-ctrl=brightness=193 \
+                          --set-ctrl=contrast=172 \
+                          --set-ctrl=saturation=143 \
+                          --set-ctrl=gain=50 \
+                          --set-ctrl=sharpness=255 \
+                          --set-ctrl=exposure_auto=1 \
+                          --set-ctrl=exposure_auto_priority=1 \
+                          --set-ctrl=exposure_absolute=156 \
+                          --set-ctrl=focus_auto=0 \
+                          --set-ctrl=zoom_absolute=200 \
+                          --set-fmt-video=width=1028 \
+                          --set-fmt-video=height=720".format(camera_id)
+output = subprocess.call(command, shell=True)
  
 while True:
 	# read the frame from the camera and send it to the server
 	try:
+		start_time = time.time()
 		frame = vs.read()
 		if hasattr(frame, 'flags'):
 			sender.send_image(rpiName, frame)
+		print('fps', 1/(time.time() - start_time))
 	except Exception as e:
 		print('error: {}'.format(e)) 
+
+
+
